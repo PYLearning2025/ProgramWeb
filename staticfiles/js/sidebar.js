46e172f4bcbbef679b6a771457e74822b7bd4598
konentung <84655730+konentung@users.noreply.github.com>
2025-02-06 10:55:03 +0800

    PROWEB-4 修改評分頁面 1. 新增sidebar的功能讓圖片不需要占據整個版面 2. 使用按鈕的方式讓圖片從側邊談出讓使用這可以查看 3. 統整程式碼把js額外設定一個檔案

---
+++ staticfiles/js/sidebar.js
@@ -0,0 +1,4 @@
+document.getElementById("sidebarToggle").addEventListener("click", function() {
+    document.getElementById("sidebar").classList.toggle("active");
+    document.getElementById("sidebarToggle").classList.toggle("active");
+});
