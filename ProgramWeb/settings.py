0dedff6f06aeca7522f8481f9703a9b5fa322354
konentung <84655730+konentung@users.noreply.github.com>
2025-09-14 10:19:53 +0800

    update: allowhost、csrftokenhost

--- ProgramWeb/settings.py
+++ ProgramWeb/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = []
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS")]
+CSRF_TRUSTED_ORIGINS = [os.environ.get("CSRF_TRUSTED_ORIGINS")]
 
 LOGIN_URL = '/login/'
 
